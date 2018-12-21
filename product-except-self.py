53178f2d7a16c512e8f17c06ffa5636135846920
mrtbld <martin.bouladour@gmail.com>
2018-12-22 00:42:07 +0100

    Improve product-except-self to linear time and space
    Linearly computes the products of other elements on the left side
    (i.e. that come before), cumulatively, for each element of the list, and
    do the same for the elements on the right with a reverse iteration.
    Then multiple both partial products lists to get the final product list.

--- product-except-self.py
+++ product-except-self.py
@@ -16,20 +16,27 @@
 # analysis.)
 
 class Solution:
-    # t:O(n²), s:O(n)
+    # t:O(n), s:O(n + output)
     def productExceptSelf(self, nums):
-        products = []
-        for i in range(len(nums)): # O(n)
-            product = 1
-            for j, n in enumerate(nums): # O(n)
-                if i != j:
-                    product *= n
-            products.append(product)
-        return products
+        left_product = 1
+        left_products = []
+        right_product = 1
+        right_products = []
+        for n in nums: # t:O(n)
+            # Build products of all elements to the left of nums[i].
+            left_products.append(left_product) # t:O(1), s:O(n)
+            left_product *= n
+        for n in reversed(nums): # t:O(n)
+            # Build products of all elements to the right of nums[i].
+            right_products.append(right_product) # t:O(1), s:O(n)
+            right_product *= n
+        # Multiply left and right products to get final products.
+        return [a * b for a, b in zip(left_products, reversed(right_products))] # s:O(output)
 
 
 solution = Solution()
 
 print(solution.productExceptSelf([1,2,3,4]), [24,12,8,6])
 print(solution.productExceptSelf([1,2,3,4,0]), [0,0,0,0,24])
+print(solution.productExceptSelf([2,2,2,2]), [8,8,8,8])
 print(len(solution.productExceptSelf([1]*10000)))
