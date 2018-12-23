9dbbb86013abfdca5709e71464756490f1afd5d5
mrtbld <martin.bouladour@gmail.com>
2018-12-23 01:21:49 +0100

    Add max-sub-array divide and conquer solution
    Add recursive solution that splits list in two at each step while
    handling max subarrays that span both halves. The linear solution with
    two accumulators is stays the most performant.
    
    Had to raise recursion limit from 1000 to 2100 so that 2000-element
    example runs.

--- max-sub-array.py
+++ max-sub-array.py
@@ -13,10 +13,24 @@
 #
 # If you have figured out the O(n) solution, try coding another solution using
 # the divide and conquer approach, which is more subtle.
+from collections import namedtuple
+import sys
+
+sys.setrecursionlimit(2100)
+
+MaxSubArrayValues = namedtuple(
+    'MaxSubArrayValues',
+    ('max_left', 'max_right', 'whole', 'max'),
+)
 
 class Solution:
-    # t:O(n), s:O(1)
     def maxSubArray(self, nums):
+        result = self.maxSubArrayAccumulator(nums)
+        assert self.maxSubArrayDivideAndConquer(nums) == result
+        return result
+
+    # t:O(n), s:O(1)
+    def maxSubArrayAccumulator(self, nums):
         if not nums:
             return None
         cur_sum = 0
@@ -29,6 +43,25 @@ class Solution:
                 cur_sum = 0
         return max_sum
 
+    def maxSubArrayDivideAndConquer(self, nums):
+        if not nums:
+            return None
+        return self.maxSubArrayDivideAndConquerRecur(nums).max
+
+    # t:O(n), s:O(n)
+    def maxSubArrayDivideAndConquerRecur(self, nums):
+        if len(nums) == 1:
+            return MaxSubArrayValues(nums[0], nums[0], nums[0], nums[0])
+        left = self.maxSubArrayDivideAndConquerRecur(nums[:len(nums)//2])
+        right = self.maxSubArrayDivideAndConquerRecur(nums[len(nums)//2:])
+        return MaxSubArrayValues(
+            max_left=max(left.max_left, left.whole + right.max_left),
+            max_right=max(right.max_right, right.whole + left.max_right),
+            whole=left.whole + right.whole,
+            max=max(left.max_right + right.max_left, left.max, right.max),
+        )
+
+
 solution = Solution()
 print(solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]), 6)
 print(solution.maxSubArray([-1]), -1)
@@ -36,4 +69,5 @@ print(solution.maxSubArray([-1,1,-1]), 1)
 print(solution.maxSubArray([-1,1,1,-1]), 2)
 print(solution.maxSubArray([0,0,1,0]), 1)
 print(solution.maxSubArray([-2,-1,-4,-2]), -1)
+print(solution.maxSubArray([31,-41,59,26,-53,58,97,-93,-23,84]), 187)
 print(solution.maxSubArray(list(range(-1000, 1000))), int((1000*(1000-1))/2))
