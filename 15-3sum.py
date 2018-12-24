a968b7789a1548f3a9b583100b3f3af0c0027060
mrtbld <martin.bouladour@gmail.com>
2018-12-24 15:20:06 +0100

    Improve 3sum performance by 1.67×
    The dict with counts is replaced with a set, and the iteration to build
    it has been removed. It's built during search loop, and counting is
    useless because the values of the couple for which we seek the opposite
    value are added to the set afterward. I.e. leverage the iteration order.
    
    Also the triple is sorted in-place, which makes code run faster:
    probably because less copying is needed, and less short-lived objects
    are created (less stress on GC).

--- 15-3sum.py
+++ 15-3sum.py
@@ -17,7 +17,6 @@
 #       [-1, 0, 1],
 #       [-1, -1, 2]
 #     ]
-from collections import defaultdict
 
 class Solution:
     # t:O(n²), s:O(n + output)
@@ -26,21 +25,19 @@ class Solution:
         >>> _ = Solution()
         >>> _.threeSum([-1, 0, 1, 2, -1, -4])
         [(-1, -1, 2), (-1, 0, 1)]
-        >>> _.threeSum(list(range(-2, 10000)))
-        [(-1, 0, 1), (-2, -1, 3), (-2, 0, 2)]
+        >>> _.threeSum(list(range(-2, 10000)) + [-3, -3])
+        [(-3, -3, 6), (-3, -2, 5), (-3, -1, 4), (-3, 0, 3), (-3, 1, 2), (-2, -1, 3), (-2, 0, 2), (-1, 0, 1)]
+        >>> _.threeSum([0] * 6000)
+        [(0, 0, 0)]
         """
-        values_counts = defaultdict(int)
-        for n in nums:
-            values_counts[n] += 1
+        seen = set() # s:O(n)
         triplets = set()
         for i, a in enumerate(nums): # t:O(n)
             for b in nums[i + 1:]: # t:O(n)
                 n = -(a+b)
-                count = values_counts[n]
-                if n == a:
-                    count -= 1
-                if n == b:
-                    count -= 1
-                if count > 0:
-                    triplets.add(tuple(sorted((n, a, b))))
-        return list(triplets)
+                if n in seen: # t:O(1)
+                    triplet = [n, a, b]
+                    triplet.sort()
+                    triplets.add(tuple(triplet))
+            seen.add(a) # t:O(1)
+        return sorted(triplets)
