b140756d6c589c988778ac96054d0f9687941fae
mrtbld <martin.bouladour@gmail.com>
2019-01-27 11:54:00 +0100

    Improve group anagram solution to linear
    Replace the sorting with a count array. This works fine because the
    alaphabet size is known, thus constant. Using an array an not a hashmap
    avoids the need to sort; the array is always in alphabet order
    regardless of the order of the chars in the strings.
    
    Using an ordered dict initialized with the alphabet chars always in the
    same order would be a acceptable alternative. It would display the same
    time and space complexities, despite most likely being slower.

--- leetcode/49-group-anagram.py
+++ leetcode/49-group-anagram.py
@@ -21,15 +21,18 @@
 from collections import defaultdict
 
 class Solution:
-    # t:O(n×k×log(k)), s:O(n×k), where k is the max length of given strings
+    # t:O(nk), s:O(nk), where k is the max length of given strings
     def groupAnagrams(self, strs):
         """Group anagrams of given array of strings.
 
         >>> Solution().groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
         [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
         """
+        offset = ord('a')
         groups = defaultdict(list)
         for s in strs:
-            key = ''.join(sorted(s))
-            groups[key].append(s)
+            key = [0]*26
+            for c in s:
+                key[ord(c) - offset] += 1
+            groups[tuple(key)].append(s)
         return list(groups.values())
