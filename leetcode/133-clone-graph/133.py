2bbc64530251516b58f803341af408eaad9a1715
mrtbld <martin.bouladour@gmail.com>
2019-03-30 23:43:58 +0100

    Move leetcode questions into txt file
    This is better for multiple solutions in different language.

--- leetcode/133-clone-graph/133.py
+++ leetcode/133-clone-graph/133.py
@@ -1,13 +1,3 @@
-# https://leetcode.com/problems/clone-graph/
-#
-# 133. Clone Graph
-#
-# Given the head of a graph, return a deep copy (clone) of the graph. Each node
-# in the graph contains a label (int) and a list (List[UndirectedGraphNode]) of
-# its neighbors. There is an edge between the given node and each of the nodes
-# in its neighbors.
-
-# Definition for a undirected graph node
 class UndirectedGraphNode:
     def __init__(self, x):
         self.label = x
--- leetcode/217-contains-duplicate/217.py
+++ leetcode/217-contains-duplicate/217.py
@@ -1,27 +1,3 @@
-# https://leetcode.com/problems/contains-duplicate/
-#
-# 217. Contains Duplicate
-#
-# Given an array of integers, find if the array contains any duplicates.
-#
-# Your function should return true if any value appears at least twice in the
-# array, and it should return false if every element is distinct.
-#
-# Example 1:
-#
-#     Input: [1,2,3,1]
-#     Output: true
-#
-# Example 2:
-#
-#     Input: [1,2,3,4]
-#     Output: false
-#
-# Example 3:
-#
-#     Input: [1,1,1,3,3,4,3,2,4,2]
-#     Output: true
-
 class Solution:
     # t:O(n), s:O(n)
     def containsDuplicate(self, nums):
